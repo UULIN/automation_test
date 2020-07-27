2312fe196dbbe6a3c18d2ae6524d432eaaf0a186
UULIN <1036190402@qq.com>
2020-07-28 00:07:56 +0800

    27/7

--- WebDriver_API/4.16调用javaScript.py
+++ WebDriver_API/4.16调用javaScript.py
@@ -1,3 +1,20 @@
 """
 webDriver的execute——script()方法执行javascript代码
+windows.scrollTO()
 """
+from time import sleep
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.set_window_size(800,600)
+driver.get("https://www.baidu.com/")
+
+
+driver.find_element_by_css_selector('#kw').send_keys("selenium")
+# driver.find_element_by_css_selector("#su").click()
+driver.find_element_by_id("su").click()
+# js1 = "window.scrollTo(100,300);"
+sleep(2)
+js1 = "window.scrollTo(100,450);"
+
+# js1 = "window.scrollTo(100,450);"
+driver.execute_script(js1)
