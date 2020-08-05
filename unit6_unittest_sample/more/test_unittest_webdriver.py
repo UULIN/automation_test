271bc8982e38023f93a4550b4d12a7b368c929c4
UULIN <1036190402@qq.com>
2020-08-05 23:00:09 +0800

    unittest_sample_ower

---
+++ unit6_unittest_sample/more/test_unittest_webdriver.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from time import sleep
+
+class test_baidu(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome()
+        cls.base_url = "https://www.baidu.com/"
+
+    def baidu_search(self,keywords):
+        self.driver.get(self.base_url)
+        self.driver.find_element_by_css_selector("#kw").send_keys(keywords)
+        self.driver.find_element_by_css_selector("#su").click()
+        sleep(2)
+
+    def test_search_selenium(self):
+        search_key = "selenium"
+        self.baidu_search(search_key)
+        self.assertEqual(self.driver.title,search_key+"_百度搜索")
+
+    def test_search_unittest(self):
+        search_key = "unittest"
+        self.baidu_search(search_key)
+        self.assertEqual(self.driver.title,search_key+"_百度搜索")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.quit()
+if __name__ == '__main__':
+    unittest.main()
